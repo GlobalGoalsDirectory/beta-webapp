b6632931bcede7908b326f6df2eb019719412ed2
Finn Woelm <public@finnwoelm.com>
2021-02-22 23:17:43 +0100

    Map: Cluster multiple icons together
    Display count of clustered icons and display a couple logos.

--- components/InteractiveMap.js
+++ components/InteractiveMap.js
@@ -2,8 +2,10 @@ import { memo } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 import Leaflet from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
-import { Box } from "@material-ui/core";
+import "react-leaflet-markercluster/dist/styles.min.css";
+import { Avatar, Box } from "@material-ui/core";
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import { ThemeProvider } from "styled-components";
 import OrganizationLogo from "components/OrganizationLogo";
@@ -52,6 +54,72 @@ const getIcon = (organization) => {
   });
 };
 
+const getClusterIcon = (cluster, organizations) => {
+  const clusterOrgs = cluster
+    .getAllChildMarkers()
+    .splice(0, 3)
+    .map((child) =>
+      organizations.find((org) => org.slug === child.options.dataSlug)
+    );
+
+  const clusterCount = cluster.getChildCount();
+
+  return Leaflet.divIcon({
+    className: "logo-cluster-icon",
+    html: renderToStaticMarkup(
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Box
+            borderRadius="100%"
+            bgcolor="black"
+            position="absolute"
+            left="50%"
+            top="50%"
+            style={{ padding: 2, margin: "-20px 0 0 -20px" }}
+            width={40}
+            height={40}
+          >
+            <Avatar style={{ width: 36, height: 36, background: "#202029" }}>
+              <Box component="span" fontSize=".9rem">
+                {clusterCount}
+              </Box>
+            </Avatar>
+          </Box>
+          <Box
+            display="flex"
+            position="absolute"
+            left={clusterCount >= 3 ? -12 : -2}
+            top={42}
+          >
+            {clusterOrgs.map((organization, index) => (
+              <Box
+                className="logo"
+                key={organization.slug}
+                borderRadius="100%"
+                bgcolor="black"
+                marginRight={index != 2 ? -1 : 0}
+                zIndex={3 - index}
+                position="relative"
+                width={26}
+                height={26}
+                style={{ padding: 1 }}
+              >
+                <OrganizationLogo
+                  variant="circle"
+                  organization={organization}
+                  size={24}
+                />
+              </Box>
+            ))}
+          </Box>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    ),
+    iconSize: [40, 70],
+    iconAnchor: [20, 35],
+    // popupAnchor: [0, -47],
+  });
+};
 const InteractiveMap = memo(({ organizations, showOrganization }) => (
   <MapContainer
     bounds={organizations.map((org) => [org.latitude, org.longitude])}
@@ -61,21 +129,25 @@ const InteractiveMap = memo(({ organizations, showOrganization }) => (
       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    {organizations.map((organization) => (
-      <Marker
-        key={organization.slug}
-        icon={getIcon(organization)}
-        position={[organization.latitude, organization.longitude]}
-        eventHandlers={{
-          click: () => {
-            console.log("click");
-            showOrganization(organization);
-          },
-        }}
-      >
-        <Popup>{organization.name}</Popup>
-      </Marker>
-    ))}
+    <MarkerClusterGroup
+      iconCreateFunction={(cluster) => getClusterIcon(cluster, organizations)}
+    >
+      {organizations.map((organization) => (
+        <Marker
+          key={organization.slug}
+          icon={getIcon(organization)}
+          position={[organization.latitude, organization.longitude]}
+          eventHandlers={{
+            click: () => {
+              showOrganization(organization);
+            },
+          }}
+          dataSlug={organization.slug}
+        >
+          <Popup>{organization.name}</Popup>
+        </Marker>
+      ))}
+    </MarkerClusterGroup>
   </MapContainer>
 ));
 

8c54f01d52930e0e30a5fb6719f19d5bcdd277bf
Finn Woelm <public@finnwoelm.com>
2021-05-25 18:49:45 +0200

    Do not copy _app.js and _document.js, but re-export content instead
    Follow the same approach for _app.js and _document.js that we have for 
    the other pages. This allows changes made to _app.js in the /templates 
    folder to be reflected on the page immediately, without having to rerun 
    the setup script.

--- scripts/setUpRoutes.js
+++ scripts/setUpRoutes.js
@@ -10,45 +10,54 @@ fse.ensureDirSync(PAGES_DIR);
 fse.emptyDirSync(PAGES_DIR);
 
 // Copy over core files
-["_app.js", "_document.js"].map((file) =>
-  fse.copySync(path.join(TEMPLATES_DIR, file), path.join(PAGES_DIR, file), {
-    overwrite: false,
-    errorOnExist: true,
-  })
-);
+PAGES = [];
+PAGES.push({
+  path: "_app.js",
+  template: "_app.js",
+});
+PAGES.push({
+  path: "_document.js",
+  template: "_document.js",
+});
 
-// Write pages for each locale
+// Copy pages for each locale
 ROUTES.forEach((route) =>
   LOCALES.forEach((locale) => {
     const templateName = route.template.replace(/\/$/, "/index");
-    const templatePath = path.join("templates", templateName);
-
     const pageName = route[locale].replace(/\/$/, "/index") + ".js";
-    const pagePath = path.join(PAGES_DIR, pageName);
 
-    // Set up required folders
-    fse.ensureDirSync(path.dirname(pagePath));
-
-    // Identify use of GSP
-    const hasGetStaticProps = fse.existsSync(
-      `${templatePath}.getStaticProps.js`
-    );
-
-    fse.writeFileSync(
-      pagePath,
-      [
-        `export { default } from "${templatePath}";`,
-        hasGetStaticProps &&
-          `export { getStaticProps, getStaticPaths } from "${templatePath}.getStaticProps";`,
-      ]
-        .filter(Boolean)
-        .join("\n\n"),
-      {
-        flag: "wx",
-      }
-    );
+    PAGES.push({
+      template: templateName,
+      path: pageName,
+    });
   })
 );
+
+// Set up pages
+PAGES.map((page) => {
+  const templatePath = path.join("templates", page.template);
+  const pagePath = path.join(PAGES_DIR, page.path);
+
+  // Set up required folders
+  fse.ensureDirSync(path.dirname(pagePath));
+
+  // Identify use of GSP
+  const hasGetStaticProps = fse.existsSync(`${templatePath}.getStaticProps.js`);
+
+  fse.writeFileSync(
+    pagePath,
+    [
+      `export { default } from "${templatePath}";`,
+      hasGetStaticProps &&
+        `export { getStaticProps, getStaticPaths } from "${templatePath}.getStaticProps";`,
+    ]
+      .filter(Boolean)
+      .join("\n\n"),
+    {
+      flag: "wx",
+    }
+  );
+});
 
 console.log(
   "Successfully set up",

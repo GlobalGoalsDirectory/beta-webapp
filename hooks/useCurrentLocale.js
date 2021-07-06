be4aa03327721ace9a841ce652f311e139d333eb
Finn Woelm <public@finnwoelm.com>
2021-07-06 11:55:14 +0200

    useCurrentLocale: Use default locale for /404 and /500 error pages

--- hooks/useCurrentLocale.js
+++ hooks/useCurrentLocale.js
@@ -4,6 +4,10 @@ import { parseUrl } from "helpers/locales";
 const useCurrentLocale = () => {
   const router = useRouter();
   const currentUrl = router.asPath;
+
+  // Default language for error pages
+  if (["/404", "/500"].includes(currentUrl)) return "en";
+
   return parseUrl(currentUrl).locale;
 };
 
